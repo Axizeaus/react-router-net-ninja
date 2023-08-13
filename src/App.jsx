5ec28becb0f2515add745e9806e7cac87b94c181
Axizeaus <axizeaus@gmail.com>
2023-08-13 10:18:46 +0630

    finished lesson 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import {
+  createBrowserRouter,
+  Route,
+  Link,
+  NavLink,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from "./components/Home";
+import About from "./components/About";
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Route>
+  )
+);
+
 const App = () => {
-  return <div className="App">Hello, Ninjas!</div>;
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  );
 };
 
 export default App;
